18121e2bf58f77078627de753ef1a206ca29b332
AmbassadorKAIFA <vladtkach925@gmail.com>
2023-01-11 15:02:32 +0200

    django_site

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -132,3 +132,6 @@ EMAIL_PORT = 587 #465
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'Site.MyUser'
